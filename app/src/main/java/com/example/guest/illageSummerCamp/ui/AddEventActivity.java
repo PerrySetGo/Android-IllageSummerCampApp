d99eccd4936002cac6b7edb8d85b7c469af0fea2
perry eising <perry.r.eising@gmail.com>
2015-10-27 15:17:37 -0700

    show options to create new event or not, reload activity

--- app/src/main/java/com/example/guest/illageSummerCamp/ui/AddEventActivity.java
+++ app/src/main/java/com/example/guest/illageSummerCamp/ui/AddEventActivity.java
@@ -1,9 +1,11 @@
 package com.example.guest.illageSummerCamp.ui;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import com.example.guest.illageSummerCamp.R;
 import com.example.guest.illageSummerCamp.adapters.EventAdapter;
@@ -19,8 +21,8 @@ public class AddEventActivity extends AppCompatActivity {
     private EditText mEventDate;
     private EditText mEventStartTime;
     private EditText mEventEndTime;
-
-
+    private Button mNewEventButton;
+    private Button mNoNewEventButton;
     private Button mSubmitButton;
     private ArrayList<Event> mEvents;
     private EventAdapter mAdapter;
@@ -32,18 +34,21 @@ public class AddEventActivity extends AppCompatActivity {
 
         mEvents =(ArrayList) Event.all();
         mAdapter = new EventAdapter(this, mEvents);
-        //setListAdapter(mAdapter);
-
-
         mEventTitle = (EditText)findViewById(R.id.editTitle);
         mEventDescription = (EditText)findViewById(R.id.editDescription);
         mEventLocation = (EditText)findViewById(R.id.editLocation);
+        final TextView startTimeLabel = (TextView)findViewById(R.id.startTimeLabel);
+        final TextView endTimeLabel = (TextView)findViewById(R.id.endTimeLabel);
         mEventDate = (EditText)findViewById(R.id.editDate);
         mEventStartTime = (EditText)findViewById(R.id.editStartTime);
         mEventEndTime = (EditText)findViewById(R.id.editEndTime);
 
-
         mSubmitButton = (Button) findViewById(R.id.eventSubmitButton);
+
+        final TextView newEventLabel = (TextView)findViewById(R.id.newEventLabel);
+        mNewEventButton = (Button)findViewById(R.id.newEventButton);
+        mNoNewEventButton = (Button)findViewById(R.id.noNewButton);
+        final TextView addNewEventLabel = (TextView)findViewById(R.id.addNewEventLabel);
 
         mSubmitButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -59,6 +64,36 @@ public class AddEventActivity extends AppCompatActivity {
                 mEvents.add(event);
                 mAdapter.notifyDataSetChanged();
 
+                mEventTitle.setVisibility(View.INVISIBLE);
+                mEventDescription.setVisibility(View.INVISIBLE);
+                mEventStartTime.setVisibility(View.INVISIBLE);
+                startTimeLabel.setVisibility(View.INVISIBLE);
+                mEventEndTime.setVisibility(View.INVISIBLE);
+                endTimeLabel.setVisibility(View.INVISIBLE);
+                mEventDate.setVisibility(View.INVISIBLE);
+                mEventLocation.setVisibility(View.INVISIBLE);
+                mSubmitButton.setVisibility(View.INVISIBLE);
+
+                newEventLabel.setVisibility(View.INVISIBLE);
+                addNewEventLabel.setVisibility(View.VISIBLE);
+                mNewEventButton.setVisibility(View.VISIBLE);
+                mNoNewEventButton.setVisibility(View.VISIBLE);
+
+                mNewEventButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        finish();
+                        startActivity(getIntent());
+                    }
+                });
+
+                mNoNewEventButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent intent = new Intent(AddEventActivity.this, MainActivity.class);
+                        startActivity(intent);
+                    }
+                });
             }
         });
 
--- app/src/main/java/com/example/guest/illageSummerCamp/ui/AllEventsActivity.java
+++ app/src/main/java/com/example/guest/illageSummerCamp/ui/AllEventsActivity.java
@@ -3,10 +3,8 @@ package com.example.guest.illageSummerCamp.ui;
 import android.app.ListActivity;
 import android.os.Bundle;
 import com.example.guest.illageSummerCamp.models.Event;
-
 import com.example.guest.illageSummerCamp.R;
 import com.example.guest.illageSummerCamp.adapters.EventAdapter;
-
 import java.util.ArrayList;
 
 public class AllEventsActivity extends ListActivity {
