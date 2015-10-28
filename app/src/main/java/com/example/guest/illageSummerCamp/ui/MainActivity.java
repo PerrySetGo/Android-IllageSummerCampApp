1eee3ed8fd431a4cc1d54c2303da1df46a78426b
Guest User <student@epicodus.com>
2015-10-28 15:31:52 -0700

    implement swipe views, Need to be moved to different button and populated dynamically still,

--- app/src/main/java/com/example/guest/illageSummerCamp/ui/MainActivity.java
+++ app/src/main/java/com/example/guest/illageSummerCamp/ui/MainActivity.java
@@ -108,7 +108,8 @@ public class MainActivity extends AppCompatActivity {
         mNextActivityButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                Intent intent = new Intent(MainActivity.this, DayEventsActivity.class);
+                startActivity(intent);
             }
         });
 
