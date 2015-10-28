446e7d6442c870966477367a8b3e78771c0066a1
Perry Eising <perry.r.eising@gmail.com>
2015-10-28 13:43:40 -0700

    add test to check if passwords match and show Toast message if false

--- app/src/main/java/com/example/guest/illageSummerCamp/ui/RegisterActivity.java
+++ app/src/main/java/com/example/guest/illageSummerCamp/ui/RegisterActivity.java
@@ -7,6 +7,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.guest.illageSummerCamp.R;
 
@@ -14,6 +15,8 @@ public class RegisterActivity extends AppCompatActivity {
 
     private EditText mNameEdit;
     private Button mLoginButton;
+    private EditText mPasswordEdit;
+    private EditText mPasswordVerify;
     private SharedPreferences mPreferences;
 
     @Override
@@ -22,6 +25,8 @@ public class RegisterActivity extends AppCompatActivity {
         setContentView(R.layout.activity_register);
 
         mNameEdit = (EditText) findViewById(R.id.nameText);
+        mPasswordEdit = (EditText)findViewById(R.id.editPassword);
+        mPasswordVerify = (EditText)findViewById(R.id.editPasswordVerify);
         mLoginButton = (Button) findViewById(R.id.registerButton);
         mPreferences = getApplicationContext().getSharedPreferences("illageCamp", Context.MODE_PRIVATE);
 
@@ -29,11 +34,22 @@ public class RegisterActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 String name = mNameEdit.getText().toString();
-                SharedPreferences.Editor editor = mPreferences.edit();
-                editor.putString("username", name);
-                editor.commit();
-                Intent intent = new Intent (RegisterActivity.this, MainActivity.class);
-                startActivity(intent);
+                String password = mPasswordEdit.getText().toString();
+                String passwordVerify = mPasswordVerify.getText().toString();
+
+                if (password.equals(passwordVerify)){// ensure accurate detection
+                    SharedPreferences.Editor editor = mPreferences.edit();
+                    editor.putString("username", name);
+                    editor.putString("password", password);
+                    editor.commit();
+                    Intent intent = new Intent (RegisterActivity.this, MainActivity.class);
+                    startActivity(intent);
+                }
+
+                else{
+                    Toast.makeText(getApplicationContext(),"Passwords don't match, please try again", Toast.LENGTH_LONG).show();
+                }
+
             }
         });
 
