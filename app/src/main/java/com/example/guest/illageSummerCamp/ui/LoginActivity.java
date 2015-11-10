5e20b51b69a9474815e81ce4acb72734593ac19d
perry eising <perry.r.eising@gmail.com>
2015-11-10 15:08:28 -0800

    successfully verify allowed user credentials against information stored in parse database.

---
+++ app/src/main/java/com/example/guest/illageSummerCamp/ui/LoginActivity.java
@@ -0,0 +1,50 @@
+package com.example.guest.illageSummerCamp.ui;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.guest.illageSummerCamp.R;
+import com.parse.LogInCallback;
+import com.parse.ParseException;
+import com.parse.ParseUser;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Bind(R.id.nameInput) EditText mNameEdit;
+    @Bind(R.id.passwordInput) EditText mPasswordEdit;
+    @Bind(R.id.loginButton) Button mLoginButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        ButterKnife.bind(this);
+
+        mLoginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //ParseUser newUser = new ParseUser();
+                ParseUser.logInInBackground(mNameEdit.getText().toString(), mPasswordEdit.getText().toString(), new LogInCallback() {
+                    public void done(ParseUser user, ParseException e) {
+                        if (user != null) {
+                            Toast.makeText(LoginActivity.this, "welcome " + ParseUser.getCurrentUser().getUsername(), Toast.LENGTH_SHORT).show();
+                            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK); //research what this does exactly
+                            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                            startActivity(intent);
+                        } else {
+                            Toast.makeText(LoginActivity.this, "Login Credentials incorrect", Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+            }
+        });
+    }
+}
