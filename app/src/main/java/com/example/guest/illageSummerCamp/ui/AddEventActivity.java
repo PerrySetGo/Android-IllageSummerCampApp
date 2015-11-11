6a25449bcb56b36fe75cf3f1aad24314a28d388b
perry eising <perry.r.eising@gmail.com>
2015-11-10 16:42:20 -0800

    create time picker and methods. add textfield to show time on add events page.

--- app/src/main/java/com/example/guest/illageSummerCamp/ui/AddEventActivity.java
+++ app/src/main/java/com/example/guest/illageSummerCamp/ui/AddEventActivity.java
@@ -1,4 +1,6 @@
 package com.example.guest.illageSummerCamp.ui;
+import android.app.Dialog;
+import android.app.TimePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -10,6 +12,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.TimePicker;
 import android.widget.Toast;
 
 import com.example.guest.illageSummerCamp.R;
@@ -33,14 +36,18 @@ public class AddEventActivity extends AppCompatActivity {
     public static final String TAG = AddEventActivity.class.getSimpleName();
 
     @Bind(R.id.editTitle) EditText mEventTitle;
-    @Bind(R.id.editDescription) EditText mEventDescription;
+    //@Bind(R.id.editDescription) EditText mEventDescription;
     @Bind(R.id.editDate) EditText mEventDate;
-    @Bind(R.id.editStartTime) EditText mEventStartTime;
-    @Bind(R.id.editEndTime) EditText mEventEndTime;
+    //@Bind(R.id.editStartTime) EditText mEventStartTime;
+    //@Bind(R.id.editEndTime) EditText mEventEndTime;
     @Bind(R.id.newEventButton) Button mNewEventButton;
     @Bind(R.id.noNewButton) Button mNoNewEventButton;
     @Bind(R.id.eventSubmitButton)  Button mSubmitButton;
     @Bind(R.id.locationSpinner) Spinner mLocationSpinner;
+    @Bind(R.id.showStartTimeButton) Button mShowTimeStartButton;
+    @Bind(R.id.startTimeView) TextView startTimeView;
+    @Bind(R.id.endTimeView) TextView endTimeView;
+
 
     private ArrayList<Event> mEvents;
     private EventAdapter mAdapter;
@@ -48,13 +55,16 @@ public class AddEventActivity extends AppCompatActivity {
     private LocationLib mLocationLib;
     public String locationChoice;
     private ArrayList<String> mLocationNames; //this is the names of the locations so we can use them for the spinner
-
+    static final int DIALOG_ID = 0;
+    int hour, minute;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_event);
         ButterKnife.bind(this);
+        showTimePickerDialog();
+
         mEvents = (ArrayList) Event.all();
         mAdapter = new EventAdapter(this, mEvents);
         mLocationNames = new ArrayList<String>();
@@ -80,8 +90,8 @@ public class AddEventActivity extends AppCompatActivity {
 
         });
 
-        final TextView startTimeLabel = (TextView) findViewById(R.id.startTimeLabel);
-        final TextView endTimeLabel = (TextView) findViewById(R.id.endTimeLabel);
+        //final TextView startTimeLabel = (TextView) findViewById(R.id.startTimeLabel);
+        //final TextView endTimeLabel = (TextView) findViewById(R.id.endTimeLabel);
         final TextView newEventLabel = (TextView) findViewById(R.id.newEventLabel);
         final TextView addNewEventLabel = (TextView) findViewById(R.id.addNewEventLabel);
 
@@ -89,29 +99,29 @@ public class AddEventActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 String eventTitle = mEventTitle.getText().toString();
-                String eventStart = mEventStartTime.getText().toString();
-                String eventEnd = mEventEndTime.getText().toString();
-                String eventDescription = mEventDescription.getText().toString();
+                //String eventStart = mEventStartTime.getText().toString();
+               // String eventEnd = mEventEndTime.getText().toString();
+                //String eventDescription = mEventDescription.getText().toString();
                 String eventDate = mEventDate.getText().toString();
-                String dateTime = eventDate + " " + eventStart;
+                //String dateTime = eventDate + " " + eventStart;
 
                 //empty fields
                 if (eventTitle.length() == 0 || eventTitle.length() == 0 || eventTitle.length() == 0 || eventTitle.length() == 0 || eventTitle.length() == 0){
                     Toast.makeText(getApplicationContext()," You need to fill out all fields to be able to save this event", Toast.LENGTH_LONG).show();
                 }
                 else{
-                    Date eventDateAsDate = getDateFromString(dateTime);
-                    Event event = new Event(eventTitle, locationChoice, eventDescription, eventDateAsDate);
-                    event.save();
-                    mEvents.add(event);
+                   // Date eventDateAsDate = getDateFromString(dateTime);
+                   // Event event = new Event(eventTitle, locationChoice, eventDescription, eventDateAsDate);
+                   // event.save();
+                    //mEvents.add(event);
                     mAdapter.notifyDataSetChanged();
 
                     mEventTitle.setVisibility(View.INVISIBLE);
-                    mEventDescription.setVisibility(View.INVISIBLE);
-                    mEventStartTime.setVisibility(View.INVISIBLE);
-                    startTimeLabel.setVisibility(View.INVISIBLE);
-                    mEventEndTime.setVisibility(View.INVISIBLE);
-                    endTimeLabel.setVisibility(View.INVISIBLE);
+                    //mEventDescription.setVisibility(View.INVISIBLE);
+                    //mEventStartTime.setVisibility(View.INVISIBLE);
+                    //startTimeLabel.setVisibility(View.INVISIBLE);
+                    //mEventEndTime.setVisibility(View.INVISIBLE);
+                   // endTimeLabel.setVisibility(View.INVISIBLE);
                     mEventDate.setVisibility(View.INVISIBLE);
                     mLocationSpinner.setVisibility(View.INVISIBLE);
                     mSubmitButton.setVisibility(View.INVISIBLE);
@@ -138,6 +148,33 @@ public class AddEventActivity extends AppCompatActivity {
                     });
                 }
 
+            }
+        });
+    }
+
+    @Override
+    protected Dialog onCreateDialog(int id){
+        if (id == DIALOG_ID)
+            return new TimePickerDialog(AddEventActivity.this,timePickerListener, hour, minute, false);
+        return null;
+    }
+
+    protected TimePickerDialog.OnTimeSetListener timePickerListener =
+            new TimePickerDialog.OnTimeSetListener() {
+            @Override
+            public void onTimeSet(TimePicker view, int hourOfDay, int minuteOfHour){
+                hour = hourOfDay;
+                minute = minuteOfHour;
+                startTimeView.setText(hourOfDay-12 + " : " + minuteOfHour);//translate to 12-hr clock
+            }
+    };
+
+
+    public void showTimePickerDialog(){
+        mShowTimeStartButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showDialog(DIALOG_ID);
             }
         });
     }
