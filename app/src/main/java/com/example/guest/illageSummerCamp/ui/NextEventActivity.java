cbb580ee9f0dd20445e0f24f5ca57dfb5647ee1a
Perry Eising <perry.r.eising@gmail.com>
2015-11-18 12:00:15 -0800

    create event Parse objects, save method, and show in list view

--- app/src/main/java/com/example/guest/illageSummerCamp/ui/NextEventActivity.java
+++ app/src/main/java/com/example/guest/illageSummerCamp/ui/NextEventActivity.java
@@ -36,23 +36,23 @@ public class NextEventActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_next_event);
         ButterKnife.bind(this);
-        Event nextEvent = Event.findRecent();
+       // Event nextEvent = Event.findRecent();
 
-        if (nextEvent != null){
-            mNextEventTitle.setText(nextEvent.getEventTitle());
-            mNextEventDescription.setText(nextEvent.getEventDescription());
-            mNextEventDate.setText(nextEvent.getDateTime().toString());
-        }
-        else{
-            mNextEventTitle.setVisibility(View.INVISIBLE);
-            mNextEventDate.setVisibility(View.INVISIBLE);
-            mDateTimeLabel.setVisibility(View.INVISIBLE);
-            mNextDescriptionLabel.setVisibility(View.INVISIBLE);
-            mNextLocationLabel.setVisibility(View.INVISIBLE);
-            mNextLabel.setVisibility(View.INVISIBLE);
-
-            mNextEventDescription.setText(R.string.no_next_event_text);
-        }
+//        if (nextEvent != null){
+//            mNextEventTitle.setText(nextEvent.getEventTitle());
+//            mNextEventDescription.setText(nextEvent.getEventDescription());
+//            mNextEventDate.setText(nextEvent.getDateTime().toString());
+//        }
+//        else{
+//            mNextEventTitle.setVisibility(View.INVISIBLE);
+//            mNextEventDate.setVisibility(View.INVISIBLE);
+//            mDateTimeLabel.setVisibility(View.INVISIBLE);
+//            mNextDescriptionLabel.setVisibility(View.INVISIBLE);
+//            mNextLocationLabel.setVisibility(View.INVISIBLE);
+//            mNextLabel.setVisibility(View.INVISIBLE);
+//
+//            mNextEventDescription.setText(R.string.no_next_event_text);
+//        }
 
 
     }
