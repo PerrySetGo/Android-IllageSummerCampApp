c26a39f9907988bd31d1744a185821fe1ba1664d
Perry Eising <perry.r.eising@gmail.com>
2015-11-17 16:17:18 -0800

    customize google map snippet by creating custom infowindow

--- app/src/main/java/com/example/guest/illageSummerCamp/ui/IllageMapActivity.java
+++ app/src/main/java/com/example/guest/illageSummerCamp/ui/IllageMapActivity.java
@@ -1,7 +1,13 @@
 package com.example.guest.illageSummerCamp.ui;
 
+import android.graphics.Color;
+import android.graphics.Typeface;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.example.guest.illageSummerCamp.R;
 import com.example.guest.illageSummerCamp.models.Location;
@@ -39,9 +45,11 @@ public class IllageMapActivity extends FragmentActivity implements OnMapReadyCal
         mLocationLib = new LocationLib();
         LatLngBounds.Builder builder = new LatLngBounds.Builder();
 
+
         for (int i = 0; i < mLocationLib.getLocations().size(); i++ ) {
-            LatLng currentLatLng = new LatLng(mLocationLib.getLocations().get(i).getLatitude(), mLocationLib.getLocations().get(i).getLongitude());
-            mMap.addMarker(new MarkerOptions().position(currentLatLng).title(mLocationLib.getLocations().get(i).getName()).snippet(mLocationLib.getLocations().get(i).getAddress()));
+            Location currentLoc = mLocationLib.getLocations().get(i);
+            LatLng currentLatLng = new LatLng(currentLoc.getLatitude(), currentLoc.getLongitude());
+            mMap.addMarker(new MarkerOptions().position(currentLatLng).title(currentLoc.getName()).snippet(currentLoc.getAddress()+ "\n\n" + currentLoc.getDescription()));
             builder.include(currentLatLng);
 
         }
@@ -51,5 +59,38 @@ public class IllageMapActivity extends FragmentActivity implements OnMapReadyCal
         int padding = 50; // offset from edges of the map in pixels
         CameraUpdate cu = CameraUpdateFactory.newLatLngBounds(bounds, padding);
         googleMap.moveCamera(cu);
+
+        mMap.setInfoWindowAdapter(new GoogleMap.InfoWindowAdapter() {
+
+            @Override
+            public View getInfoWindow(Marker arg0) {
+                return null;
+            }
+
+            @Override
+            public View getInfoContents(Marker marker) {
+
+                LinearLayout info = new LinearLayout(getApplicationContext());
+                info.setOrientation(LinearLayout.VERTICAL);
+
+                TextView title = new TextView(getApplicationContext());
+                title.setTextColor(Color.BLACK);
+                title.setGravity(Gravity.CENTER);
+                title.setTypeface(null, Typeface.BOLD);
+                title.setText(marker.getTitle());
+
+                TextView snippet = new TextView(getApplicationContext());
+                snippet.setTextColor(Color.GRAY);
+                snippet.setText(marker.getSnippet());
+                //find a way to put image in snippet? http://stackoverflow.com/questions/18938187/add-an-image-from-url-into-custom-infowindow-google-maps-v2
+
+                info.addView(title);
+                info.addView(snippet);
+
+                return info;
+            }
+        });
     }
+
+
 }
