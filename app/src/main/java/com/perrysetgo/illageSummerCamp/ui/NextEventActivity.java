7b69b7cb096918d5b600ae04d09fc135bb94c36a
perry eising <perry.r.eising@gmail.com>
2016-06-30 16:26:04 -0700

    next event display improved signficantly - now accurately detects events that start in the next 3 hrs.

--- app/src/main/java/com/perrysetgo/illageSummerCamp/ui/NextEventActivity.java
+++ app/src/main/java/com/perrysetgo/illageSummerCamp/ui/NextEventActivity.java
@@ -31,77 +31,77 @@ public class NextEventActivity extends AppCompatActivity {
     private Event nextEvent;
     long rightNowInMillis = Calendar.getInstance().getTimeInMillis();
     long eventWindowEnd = rightNowInMillis + WINDOW_END; //3hrs from now.
-    @Bind(R.id.nextEventTitle)
-    TextView mNextEventTitle;
-    @Bind(R.id.nextEventDescription)
-    TextView mNextEventDescription;
-    @Bind(R.id.nextEventDate)
-    TextView mNextEventDate;
-    @Bind(R.id.nextLocationLabel)
-    TextView mNextLocationLabel;
-    @Bind(R.id.nextEventLabel)
-    TextView mNextEventLabel;
-    @Bind(R.id.nextDescriptionLabel)
-    TextView mNextDescriptionLabel;
+    @Bind(R.id.nextEventTitleBox)
+    TextView nextEventTitleBox;
+    @Bind(R.id.nextEventTitleLabel)
+    TextView nextEventTitleLabel;
+
+
+    @Bind(R.id.nextEventLocationBox)
+    TextView nextEventLocationBox;
+    @Bind(R.id.nextEventLocationLabel)
+    TextView nextEventLocationLabel;
+
+    @Bind(R.id.nextEventDateTimeBox)
+    TextView nextEventDateTimeBox;
     @Bind(R.id.nextDateTimeLabel)
-    TextView mDateTimeLabel;
-    @Bind(R.id.nextLabel)
-    TextView mNextLabel;
+    TextView nextEventDateTimeLabel;
+
+    @Bind(R.id.nextEventDescriptionLabel)
+    TextView nextEventDescriptionLabel;
+    @Bind(R.id.nextEventDescriptionBox)
+    TextView nextEventDescriptionBox;
+
+    @Bind(R.id.eventsStatusBox)
+    TextView eventsStatusBox;
+
 
     public static final String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_next_event);
-        ButterKnife.bind(this);
 
-        findUpcoming(); //should return event if it is present in window
-        Log.i(TAG, nextEvent.getEventDescription());
-
-        if (nextEvent != null){
-            Log.i(TAG, "success!");
-            mNextEventTitle.setText(nextEvent.getEventTitle());
-            mNextEventDescription.setText(nextEvent.getEventDescription());
-
-            SimpleDateFormat startTimeFormat = new SimpleDateFormat("EEE, MM/dd hh:mm a", Locale.US);
-            String startDateTime = startTimeFormat.format(nextEvent.getEventEndDateTime());
-
-            mNextEventDate.setText(startDateTime);
-
-
-        }
-        else{
-            mNextEventTitle.setVisibility(View.INVISIBLE);
-            mNextEventDate.setVisibility(View.INVISIBLE);
-            mDateTimeLabel.setVisibility(View.INVISIBLE);
-            mNextDescriptionLabel.setVisibility(View.INVISIBLE);
-            mNextLocationLabel.setVisibility(View.INVISIBLE);
-            mNextLabel.setVisibility(View.INVISIBLE);
-
-            mNextEventDescription.setText(R.string.no_next_event_text);
-        }
-
-
-   }
-
-
-    public void findUpcoming() { //instead of doing work here, pull all events and then loop thrugh them to find future event.
         DatabaseReference ref = FirebaseDatabase.getInstance().getReference(Constants.FIREBASE_CHILD_EVENTS);
-        Query queryRef = ref.orderByValue();
+        Query queryRef = ref.orderByChild("eventStartDateTime");
+
         queryRef.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
                 for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
-                    Event checkEvent = snapshot.getValue(Event.class);
-                    Log.i(TAG, checkEvent.getEventDescription());
-                    if (!(checkEvent.getEventStartDateTime() > rightNowInMillis && checkEvent.getEventStartDateTime() < eventWindowEnd)) {
-                        Log.i(TAG, "not in window!");
-                        nextEvent = null;
+                    Event nextEvent = snapshot.getValue(Event.class);
+                    if (!(nextEvent.getEventStartDateTime() > rightNowInMillis && nextEvent.getEventStartDateTime() < eventWindowEnd)) {
+                        nextEventTitleBox.setVisibility(View.INVISIBLE);
+                        nextEventDateTimeBox.setVisibility(View.INVISIBLE);
+                        nextEventDateTimeLabel.setVisibility(View.INVISIBLE);
+                        nextEventDescriptionLabel.setVisibility(View.INVISIBLE);
+                        nextEventLocationLabel.setVisibility(View.INVISIBLE);
+                        eventsStatusBox.setVisibility(View.VISIBLE);
+                        eventsStatusBox.setText(R.string.no_next_event_text);
+                        nextEventDescriptionBox.setVisibility(View.INVISIBLE);
                         continue;
                     } else {
-                        Log.i(TAG, "in window");
-                        nextEvent = checkEvent;
+                        nextEventTitleLabel.setVisibility(View.VISIBLE);
+                        nextEventTitleBox.setText(nextEvent.getEventTitle());
+                        nextEventTitleBox.setVisibility(View.VISIBLE);
+
+                        nextEventLocationLabel.setVisibility(View.VISIBLE);
+                        nextEventLocationBox.setText(nextEvent.getEventLocation());
+                        nextEventLocationBox.setVisibility(View.VISIBLE);
+
+                        SimpleDateFormat startTimeFormat = new SimpleDateFormat("EEE, MM/dd hh:mm a", Locale.US);
+                        String startDateTime = startTimeFormat.format(nextEvent.getEventEndDateTime());
+                        nextEventDateTimeBox.setText(startDateTime);
+                        nextEventDateTimeLabel.setVisibility(View.VISIBLE);
+                        nextEventDateTimeBox.setVisibility(View.VISIBLE);
+
+                        nextEventDescriptionLabel.setVisibility(View.VISIBLE);
+                        nextEventDescriptionBox.setText(nextEvent.getEventDescription());
+                        nextEventDescriptionBox.setVisibility(View.VISIBLE);
+
+                        eventsStatusBox.setVisibility(View.INVISIBLE);
+
+                        return;
                     }
                 }
             }
@@ -111,6 +111,7 @@ public class NextEventActivity extends AppCompatActivity {
                 Log.d(TAG, error.toString());
             }
         });
-
-    }
+        setContentView(R.layout.activity_next_event);
+        ButterKnife.bind(this);
+   }
 }
