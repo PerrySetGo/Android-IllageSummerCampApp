c567199940344cfcb89e63a063da66a3312d88a8
Perry Eising <perry.r.eising@gmail.com>
2016-07-07 16:12:33 -0700

    rewrite login logic. unclear if working, must recreate logout logic first.

--- app/src/main/java/com/perrysetgo/illageSummerCamp/Constants.java
+++ app/src/main/java/com/perrysetgo/illageSummerCamp/Constants.java
@@ -6,7 +6,7 @@ package com.perrysetgo.illageSummerCamp;
 public class Constants {
 
     public static final String FIREBASE_CHILD_EVENTS = "events";
-    public static final String GMAPS_API_KEY = BuildConfig.GMAPS_API_KEY;
     public static final String PREFERENCES_USER_KEY = "user";
     public static final String PREFERENCES_PW_KEY = "pw";
+    public static final String PREFERENCES_LOGIN_STATUS = "false";
 }
--- app/src/main/java/com/perrysetgo/illageSummerCamp/IllageApplication.java
+++ app/src/main/java/com/perrysetgo/illageSummerCamp/IllageApplication.java
@@ -1,10 +1,20 @@
 package com.perrysetgo.illageSummerCamp;
 
 
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+
 public class IllageApplication extends com.activeandroid.app.Application {
+    private SharedPreferences mSharedPreferences;
+    private SharedPreferences.Editor mEditor;
 
     @Override
     public void onCreate() {
         super.onCreate();
+        mSharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        mEditor = mSharedPreferences.edit();
+        mEditor.putString(Constants.PREFERENCES_USER_KEY, "goodChoices").apply();
+        mEditor.putString(Constants.PREFERENCES_PW_KEY, "l34d3r").apply();
+        mEditor.putBoolean(Constants.PREFERENCES_LOGIN_STATUS, false);
     }
 }
