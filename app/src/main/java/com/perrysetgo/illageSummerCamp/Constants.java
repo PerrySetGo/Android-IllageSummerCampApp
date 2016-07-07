69061955837150d13f81281bdd3fd416e68dd17a
Perry Eising <perry.r.eising@gmail.com>
2016-07-07 15:30:05 -0700

    working towards having a revamped login procedure

--- app/src/main/java/com/perrysetgo/illageSummerCamp/Constants.java
+++ app/src/main/java/com/perrysetgo/illageSummerCamp/Constants.java
@@ -7,4 +7,6 @@ public class Constants {
 
     public static final String FIREBASE_CHILD_EVENTS = "events";
     public static final String GMAPS_API_KEY = BuildConfig.GMAPS_API_KEY;
+    public static final String PREFERENCES_USER_KEY = "user";
+    public static final String PREFERENCES_PW_KEY = "pw";
 }
