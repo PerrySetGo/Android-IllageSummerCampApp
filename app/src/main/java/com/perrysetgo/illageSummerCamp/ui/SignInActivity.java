1d335cbfced8c8500dcd9bb98ab8132fb052c782
perry eising <perry.r.eising@gmail.com>
2016-07-11 16:59:42 -0700

    edit clicklistener

--- app/src/main/java/com/perrysetgo/illageSummerCamp/ui/SignInActivity.java
+++ app/src/main/java/com/perrysetgo/illageSummerCamp/ui/SignInActivity.java
@@ -2,10 +2,16 @@ package com.perrysetgo.illageSummerCamp.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import com.perrysetgo.illageSummerCamp.R;
 
-public class SignInActivity extends AppCompatActivity {
+import butterknife.Bind;
+
+public class SignInActivity extends AppCompatActivity  {
+    @Bind(R.id.registerTextView)
+    TextView registerTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
