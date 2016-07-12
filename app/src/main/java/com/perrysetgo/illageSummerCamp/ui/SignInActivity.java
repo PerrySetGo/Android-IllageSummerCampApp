a4609ac38a20a5fa2e894f159e054cab8be9c01e
perry eising <perry.r.eising@gmail.com>
2016-07-12 14:07:59 -0700

    towards signup/signin activities

--- app/src/main/java/com/perrysetgo/illageSummerCamp/ui/SignInActivity.java
+++ app/src/main/java/com/perrysetgo/illageSummerCamp/ui/SignInActivity.java
@@ -1,5 +1,6 @@
 package com.perrysetgo.illageSummerCamp.ui;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -8,8 +9,9 @@ import android.widget.TextView;
 import com.perrysetgo.illageSummerCamp.R;
 
 import butterknife.Bind;
+import butterknife.ButterKnife;
 
-public class SignInActivity extends AppCompatActivity  {
+public class SignInActivity extends AppCompatActivity  implements View.OnClickListener{
     @Bind(R.id.registerTextView)
     TextView registerTextView;
 
@@ -17,5 +19,18 @@ public class SignInActivity extends AppCompatActivity  {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+
+        ButterKnife.bind(this);
+        registerTextView.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v){
+            if (v == registerTextView){
+                Intent intent = new Intent (SignInActivity.this, SignUpActivity.class);
+                startActivity(intent);
+                finish();
+            }
     }
 }
