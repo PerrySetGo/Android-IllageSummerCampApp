131933166b84604f09e3af032fe9767196a8cfaa
perry eising <perry.r.eising@gmail.com>
2016-07-12 14:07:59 -0700

    towards signup/signin activities

--- app/src/main/java/com/perrysetgo/illageSummerCamp/ui/SignUpActivity.java
+++ app/src/main/java/com/perrysetgo/illageSummerCamp/ui/SignUpActivity.java
@@ -1,14 +1,80 @@
 package com.perrysetgo.illageSummerCamp.ui;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 import com.perrysetgo.illageSummerCamp.R;
 
-public class SignUpActivity extends AppCompatActivity {
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class SignUpActivity extends AppCompatActivity implements View.OnClickListener {
+    public static final String TAG = SignUpActivity.class.getSimpleName();
+
+    private FirebaseAuth auth;
+
+    @Bind(R.id.createUserButton) Button createUserButton;
+    @Bind(R.id.nameEditText) EditText nameEditText;
+    @Bind(R.id.emailEditText) EditText emailEditText;
+    @Bind(R.id.passwordEditText) EditText passwordEditText;
+    @Bind(R.id.confirmPasswordEditText) EditText confirmPasswordEditText;
+    @Bind(R.id.loginTextView) TextView loginTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+        auth = FirebaseAuth.getInstance();
+        ButterKnife.bind(this);
+    }
+
+    @Override
+    public void onClick(View view){
+
+        if (view == loginTextView){
+            Intent intent = new Intent (SignUpActivity.this, SignInActivity.class); //manage activity stack
+                                                                                    //custom control back button
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            startActivity(intent);
+            finish();
+        }
+        if (view == createUserButton){
+            createNewUser();
+        }
+
+    }
+
+    private void createNewUser(){
+        final String name = nameEditText.getText().toString().trim();
+        final String email = emailEditText.getText().toString().trim();
+        String password = passwordEditText.toString().trim();
+        String confirmPassword = confirmPasswordEditText.getText().toString().trim();
+
+        auth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>(){
+                    @Override
+                    public void onComplete (@NonNull Task<AuthResult> task){
+                        if (task.isSuccessful()){
+                            Log.i(TAG, "success");
+                        }
+                        else {
+                            Toast.makeText(SignUpActivity.this, "Failed", Toast.LENGTH_LONG).show();
+                        }
+                    }
+
+                });
+
     }
 }
